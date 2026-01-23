907b82e24f6da1d724a87ce91227371c5a2a1e69
Lihus <75552840+lihuss@users.noreply.github.com>
2026-01-23 19:59:10 +0800

    Newui (#25)
    * 优化发帖文字前空格过多的问题，以及更换更优的CDN服务
    
    * 优化文字前空格问题、图片预览问题，取消了CDN服务，增加了用户登陆的严谨性
    
    * Change ui style.
    
    ---------
    
    Co-authored-by: Yerdan <Yerdan@qq.com>

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // 创建连接池 (比单次连接更高效，适合网站)
 const pool = mysql.createPool({
-    host: 'localhost',      // 数据库地址
+    host: '112.74.54.5',      // 数据库地址
     user: 'root',           // 数据库账号 (通常是 root)
     password: 'Zqzx2025@Start!',       // 【注意】这里填你的数据库密码，如果是phpStudy通常是root
     database: 'zz_memory_2025',
